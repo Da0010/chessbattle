04d38e0899b3c8ff37e94a81aaab2af3a2317111
Da0010 <jidaoyi29@gmail.com>
2023-09-26 18:17:14 -0700

    Add SE Voume Slider

--- Assets/script/GameScr/MakeSE.cs
+++ Assets/script/GameScr/MakeSE.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine;
+using UnityEngine.UI;
 public class MakeSE : MonoBehaviour
 {
     [SerializeField] AudioClip moveClip;
     [SerializeField] AudioClip killedClip;
     [SerializeField] AudioClip checkClip;
     [SerializeField] AudioClip timeOverClip;
+    [SerializeField] AudioClip SEVolumeClip;
+
+	[SerializeField] Slider SESlider;
     AudioSource SE;
     // Start is called before the first frame update
     void Start()
     {
         SE = this.GetComponent<AudioSource>();
+        SESlider.onValueChanged.AddListener(value => SE.volume = value);
+    }
+
+    public void shotSEVolumeSound(){
+        SE.PlayOneShot(SEVolumeClip);
     }
     public void shotMoveSound(){
         SE.PlayOneShot(moveClip);
